8c59743bd2d37d566be748765b1ef5ccfe6c68c3
nivethika <nivemaham@gmail.com>
2020-05-20 10:33:11 +0200

    attempt to fix notification and regenerated schedule

--- src/assets/data/defaultConfig.ts
+++ src/assets/data/defaultConfig.ts
@@ -82,7 +82,7 @@ export const DefaultNotificationType: string = 'FCM'
 export const FCMPluginProjectSenderId: string = '776941468489'
 
 // *Default maximum upstream retries when sending fails
-export const DefaultMaxUpstreamResends = 100
+export const DefaultMaxUpstreamResends = 50
 
 // *Default number of notifications to schedule
 export const DefaultNumberOfNotificationsToSchedule: number = 100
