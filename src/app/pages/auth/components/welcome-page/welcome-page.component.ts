2e8736f9108fa6c3e4cb0976ba0eb5e33c3d7763
peyman-mohtashami <p.mohtashami@gmail.com>
2020-07-23 23:08:31 +0200

    Add Select language button to Welcome page

--- src/app/pages/auth/components/welcome-page/welcome-page.component.ts
+++ src/app/pages/auth/components/welcome-page/welcome-page.component.ts
@@ -3,7 +3,7 @@ import { NavController, Slides } from 'ionic-angular'
 
 import {
   DefaultLanguage,
-  DefaultSettingsSupportedLanguages
+  DefaultSettingsSupportedLanguages, LanguageMap
 } from '../../../../../assets/data/defaultConfig'
 import { AuthConfigService } from '../../../../core/services/config/auth-config.service'
 import { ConfigService } from '../../../../core/services/config/config.service'
@@ -13,11 +13,13 @@ import { LogService } from '../../../../core/services/misc/log.service'
 import { StorageService } from '../../../../core/services/storage/storage.service'
 import { UsageService } from '../../../../core/services/usage/usage.service'
 import { EnrolmentEventType } from '../../../../shared/enums/events'
-import { LanguageSetting } from '../../../../shared/models/settings'
+import {LanguageSetting, Settings} from '../../../../shared/models/settings'
 import { HomePageComponent } from '../../../home/containers/home-page.component'
 import { SplashPageComponent } from '../../../splash/containers/splash-page.component'
 import { AuthService } from '../../services/auth.service'
 import { EligibilityPageComponent } from '../eligibility-page/eligibility-page.component'
+import {LocKeys} from "../../../../shared/enums/localisations";
+import {SettingsService} from "../../../settings/services/settings.service";
 
 /**
  * Generated class for the WelcomePage page.
@@ -41,6 +43,9 @@ export class WelcomePageComponent {
   showReadMoreInformation: boolean = false
   languagesSelectable: LanguageSetting[] = DefaultSettingsSupportedLanguages
 
+  settings: Settings = {}
+  showLoading = false
+
   constructor(
     private navCtrl: NavController,
     private localization: LocalizationService,
@@ -51,7 +56,8 @@ export class WelcomePageComponent {
     private usage: UsageService,
     private logger: LogService,
     private config: ConfigService,
-    private authConfig: AuthConfigService
+    private authConfig: AuthConfigService,
+    private settingsService: SettingsService,
   ) {}
 
   ionViewDidLoad() {
@@ -60,39 +66,38 @@ export class WelcomePageComponent {
 
   ionViewDidEnter() {}
 
-  // Keeping this in the code since we will need it for phase 2
-  // showSelectLanguage() {
-  //   const buttons = [
-  //     {
-  //       text: this.localization.translateKey(LocKeys.BTN_CANCEL),
-  //       handler: () => {}
-  //     },
-  //     {
-  //       text: this.localization.translateKey(LocKeys.BTN_SET),
-  //       handler: selectedLanguageVal => {
-  //         const lang: LanguageSetting = {
-  //           label: LanguageMap[selectedLanguageVal],
-  //           value: selectedLanguageVal
-  //         }
-  //         this.localization.setLanguage(lang).then(() => {
-  //           this.language = lang
-  //           return this.navCtrl.setRoot(EnrolmentPageComponent)
-  //         })
-  //       }
-  //     }
-  //   ]
-  //   const inputs = this.languagesSelectable.map(lang => ({
-  //     type: 'radio',
-  //     label: this.localization.translate(lang.label),
-  //     value: lang.value,
-  //     checked: lang.value === this.language.value
-  //   }))
-  //   return this.alertService.showAlert({
-  //     title: this.localization.translateKey(LocKeys.SETTINGS_LANGUAGE_ALERT),
-  //     buttons: buttons,
-  //     inputs: inputs
-  //   })
-  // }
+  showSelectLanguage() {
+    const buttons = [
+      {
+        text: this.localization.translateKey(LocKeys.BTN_CANCEL),
+        handler: () => {}
+      },
+      {
+        text: this.localization.translateKey(LocKeys.BTN_SET),
+        handler: selectedLanguageVal => {
+          const lang: LanguageSetting = {
+            label: LanguageMap[selectedLanguageVal],
+            value: selectedLanguageVal
+          }
+          this.localization.setLanguage(lang).then(() => {
+            this.language = lang
+            return this.backToSplash()
+          })
+        }
+      }
+    ]
+    const inputs = this.languagesSelectable.map(lang => ({
+      type: 'radio',
+      label: this.localization.translate(lang.label),
+      value: lang.value,
+      checked: lang.value === this.language.value
+    }))
+    return this.alertService.showAlert({
+      title: this.localization.translateKey(LocKeys.SETTINGS_LANGUAGE_ALERT),
+      buttons: buttons,
+      inputs: inputs
+    })
+  }
 
   joinStudy() {
     this.reset()
@@ -100,7 +105,10 @@ export class WelcomePageComponent {
   }
 
   reset() {
-    return this.config.resetAll().then(() => this.authConfig.init())
+    const language = this.localization.getLanguage()
+    return this.config.resetAll().then(() => {
+      this.localization.setLanguage(language).then(() => this.authConfig.init())
+    })
   }
 
   goToLogin() {
@@ -168,4 +176,9 @@ export class WelcomePageComponent {
   showReadMoreText() {
     this.showReadMoreInformation = true
   }
+
+  backToSplash() {
+    this.navCtrl.setRoot(SplashPageComponent)
+  }
+
 }
